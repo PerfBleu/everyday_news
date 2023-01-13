ecbfee83c6e5cf5718f2a7e2244d8999fc69a000
PerfBleu <lunaticstarksm@gmail.com>
2023-01-13 22:29:44 +0800

    测试分群控制

---
+++ config.py
@@ -0,0 +1,45 @@
+from configparser import RawConfigParser, NoOptionError
+import os
+from pathlib import Path
+
+working_path = os.path.dirname(__file__)
+cfgpath = working_path / 'config.ini'
+config = RawConfigParser()
+config.read(cfgpath)
+default_setting = [True]
+
+def get_group_config_byindex(gid: int, index: int) -> bool:
+    return get_group_config(gid=gid)[index]
+
+def set_group_config_byindex(gid: int, index: int, setting: bool):
+    cfg =  get_group_config(gid)
+    cfg[index] = setting
+    write_group_config(gid, encode_setting(cfg))
+    
+def set_default_config(gid: int):
+    write_group_config(gid, encode_setting(default_setting))
+
+def get_group_config(gid: int) -> list:
+    try:
+        setting_str = config.get('GROUP_CONFIG', str(gid))
+    except NoOptionError:
+        set_default_config(gid)
+        return default_setting
+    return(parse_setting(settingstr=setting_str))
+
+def write_group_config(gid: int, setting_str: str):
+    config.set('GROUP_CONFIG', str(gid), setting_str)
+    with open(cfgpath, 'w') as f:
+        config.write(f)
+
+def parse_setting(settingstr: str) -> list:
+    setting_list = []
+    for i in settingstr:
+        setting_list.append(True if i == 1 else False)
+    return(setting_list)
+
+def encode_setting(cfglist: list) -> str:
+    string = ''
+    for cfg in cfglist:
+        string += ('1' if cfg else '0')
+    return(string)
--- everyday_news.py
+++ everyday_news.py
@@ -1,11 +1,15 @@
 __version__ = '0.2.0'
 import os
-from hoshino import Service
+from hoshino import Service, get_self_ids, priv
 from hoshino import aiorequests as req
 from hoshino.util import pic2b64
+from nonebot import get_bot
 from PIL import Image
 from io import BytesIO
 import json
+import asyncio
+
+from .config import get_group_config_byindex, set_group_config_byindex
 
 sv = Service('everydayNews', visible= True, enable_on_default= True, bundle='everydayNews', help_='''
 每日简报
@@ -13,7 +17,6 @@ sv = Service('everydayNews', visible= True, enable_on_default= True, bundle='eve
 path = os.path.dirname(__file__)
 
 async def getPicUrl():
-    # api修复
     getObject = await req.get('http://118.31.18.68:8080/news/api/news-file/get')
     if getObject.status_code != 200:
         sv.logger.warning('URL获取失败')
@@ -32,8 +35,7 @@ async def getImg():
         return()
     else:
         sv.logger.info('图片获取成功')
-    # with open(os.path.join(path, "tmp.jpg"),'wb') as image:
-    #     image.write(await getObject.content)
+
     image = Image.open(BytesIO(await getObject.content))
     return f'[CQ:image,file={pic2b64(image)}]'
 
@@ -44,8 +46,33 @@ async def news(bot,ev):
     # tmppath = os.path.join(path, "tmp.jpg")
     await bot.send(ev, img, at_sender=True)
 
+# @sv.scheduled_job('cron', hour = '9')
+# async def news_scheduled():
+#     img = await getImg()
+#     # tmppath = os.path.join(path, "tmp.jpg")
+#     await sv.broadcast(img,'auto_send_news_message', 2)
+
 @sv.scheduled_job('cron', hour = '9')
 async def news_scheduled():
     img = await getImg()
-    # tmppath = os.path.join(path, "tmp.jpg")
-    await sv.broadcast(img,'auto_send_news_message', 2)
+    bot = get_bot()
+    sids = get_self_ids()
+    for sid in sids:
+        for gid in bot.get_group_list(self_id = sid):
+            if get_group_config_byindex(gid):
+                bot.send_group_msg(self_id = sid, group_id = gid, message = img)
+                asyncio.sleep(1000)
+
+@sv.on_fullmatch(('停止简报推送','禁用简报推送'))
+async def news_off(bot,ev):
+    if not priv.check_priv(ev, priv.ADMIN):
+        await bot.finish(ev,"权限不足")
+    set_group_config_byindex(gid=ev.group_id, index=0, setting=False)
+    await bot.send(ev, '本群简报推送已禁用', at_sender=True)
+
+@sv.on_fullmatch(('启用简报推送','开启简报推送'))
+async def news_on(bot,ev):
+    if not priv.check_priv(ev, priv.ADMIN):
+        await bot.finish(ev,"权限不足")
+    set_group_config_byindex(gid=ev.group_id, index=0, setting=True)
+    await bot.send(ev, '本群简报推送已启用', at_sender=True)
